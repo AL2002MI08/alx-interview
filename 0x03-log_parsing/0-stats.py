01c875653424d3fd181f788dec1ebc494014145c
Alexandra_08 <105152394+AL2002MI08@users.noreply.github.com>
2023-08-17 10:52:25 +0200

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,38 +1,45 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+"""Script that reads stdin line by line and computes metrics"""
 
 import sys
 
-# Initialize counters
-total_size = 0
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+def printsts(dic, size):
+    """ WWPrints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
+
+
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
 
 try:
-    # Read lines from standard input
-    for i, line in enumerate(sys.stdin):
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printsts(sts, size)
+
+        stlist = line.split()
+        count += 1
+
         try:
-            # Parse line into components
-            ip_address, _, _, timestamp, _, method, resource, protocol, status_code, file_size = line.split()
+            size += int(stlist[-1])
+        except:
+            pass
 
-            # Increment counters
-            total_size += int(file_size)
-            status_codes[int(status_code)] += 1
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printsts(sts, size)
 
-            # Print statistics every 10 lines
-            if i % 10 == 9:
-                print("File size: {}".format(total_size))
-                for code in sorted(status_codes):
-                    if status_codes[code] > 0:
-                        print("{}: {}".format(code, status_codes[code]))
-                print()
-
-        except ValueError:
-            # Skip lines that do not match expected format
-            continue
 
 except KeyboardInterrupt:
-    # Print final statistics on interrupt
-    print("File size: {}".format(total_size))
-    for code in sorted(status_codes):
-        if status_codes[code] > 0:
-            print("{}: {}".format(code, status_codes[code]))
-    sys.exit(0)
+    printsts(sts, size)
+    raise
