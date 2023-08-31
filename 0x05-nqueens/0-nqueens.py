63201c7095ebca8b8cd3a496770d8dc1d181ea91
Alexandra_08 <105152394+AL2002MI08@users.noreply.github.com>
2023-08-31 19:42:04 +0200

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,23 +2,6 @@
 """Solving the N Queens problem"""
 import sys
 
-
-args = sys.argv
-
-if len(args) != 2:
-    print("Usage: nqueens N")
-    exit(1)
-
-if args[1].isdigit() is False:
-    print("N must be a number")
-    exit(1)
-
-layout = int(args[1])
-if layout < 4:
-    print("N must be at least 4")
-    exit(1)
-
-
 def check_here(nlist, index, curr_num):
     for item in nlist:
         if index == item[1] or\
@@ -27,9 +10,8 @@ def check_here(nlist, index, curr_num):
             return 1
     return list([curr_num, index])
 
-
-def loop_n_find(nlist, curr_num):
-    if (curr_num > layout):
+def loop_n_find(nlist, curr_num, layout):
+    if curr_num > layout:
         return
 
     if len(nlist) == layout:
@@ -42,11 +24,26 @@ def loop_n_find(nlist, curr_num):
         if type(val) is list:
             new_nlist = nlist.copy()
             new_nlist.append(val)
-            loop_n_find(new_nlist, curr_num + 1)
+            loop_n_find(new_nlist, curr_num + 1, layout)
         j += 1
     return
 
+if __name__ == "__main__":
+    args = sys.argv
 
-for i in range(layout):
-    arr = [[0, i]]
-    loop_n_find(arr, 1)
+    if len(args) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+
+    if not args[1].isdigit():
+        print("N must be a number")
+        exit(1)
+
+    layout = int(args[1])
+    if layout < 4:
+        print("N must be at least 4")
+        exit(1)
+
+    for i in range(layout):
+        arr = [[0, i]]
+        loop_n_find(arr, 1, layout)
