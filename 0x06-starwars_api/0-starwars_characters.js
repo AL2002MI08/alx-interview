ca4fb257f9f4daf76280ca5227d0006376215656
Alexandra_08 <105152394+AL2002MI08@users.noreply.github.com>
2023-09-08 00:58:05 +0200

    create 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -7,19 +7,13 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
 
 request(url, async (err, res, body) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+  err && console.log(err);
 
-  const charactersArray = JSON.parse(body).characters;
+  const charactersArray = (JSON.parse(res.body).characters);
   for (const character of charactersArray) {
     await new Promise((resolve, reject) => {
       request(character, (err, res, body) => {
-        if (err) {
-          console.error(err);
-          return reject(err);
-        }
+        err && console.log(err);
 
         console.log(JSON.parse(body).name);
         resolve();
